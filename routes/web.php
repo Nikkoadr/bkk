35e7d4ee7cca4d8d958783d184cbf059bc923845
Nikko Adrian <nikkoadrian02@gmail.com>
2024-05-30 14:26:29 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ Auth::routes(['register' => false]);
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::post('/form_daftar', [PendaftaranController::class, 'form_daftar'])->name('form_daftar');
 Route::post('/daftar', [PendaftaranController::class, 'daftar'])->name('daftar');
+Route::get('/daftar', function () {
+    return redirect('/');});
 Route::get('/bukti_pembayaran/{id}', [PendaftaranController::class, 'bukti_pembayaran'])->name('bukti_pembayaran');
 Route::get('/data_loker', [HomeController::class, 'data_loker'])->name('data_loker');
 Route::get('/data_pelamar', [HomeController::class, 'data_pelamar'])->name('data_pelamar');

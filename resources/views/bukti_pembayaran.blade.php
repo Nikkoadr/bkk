bdee2fac352baef5c14ad5a611d3ecccfe0bdd7b
Nikko Adrian <nikkoadrian02@gmail.com>
2024-06-06 11:27:14 +0700

    update method

--- resources/views/bukti_pembayaran.blade.php
+++ resources/views/bukti_pembayaran.blade.php
@@ -85,7 +85,7 @@
                                     <p class="text-center"><b>Pada tanggal : {{ Carbon\Carbon::parse($pendaftaran->pendaftaran_created_at)->setTimezone('Asia/Jakarta')->translatedFormat('d F Y, H:i') }}</b></p>
                                     <p class="text-center">Simpan bukti pendaftaran ini. Sebagai syarat mengikuti proses recruitment perusahaan.</p>
                                     <p class="text-center m-3">{!! QrCode::size(100)->backgroundColor(255,255,255)->generate('https://bkk.smkmuhkandanghaur.sch.id/cari/'.$pendaftaran->code_pendaftaran) !!}</p>
-                                    <form action="print_bukti_transfer/{{ $pendaftaran -> code_pendaftaran }}" method="post">
+                                    <form action="print_bukti_transfer/{{ $pendaftaran -> code_pendaftaran }}" method="get">
                                     @csrf
                                         <p class="text-center"><button class="btn btn-primary" formtarget="_blank" type="submit" >Print</button></p>
                                     </form>

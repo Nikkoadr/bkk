c51ef61524dc19bf3a5f5185a8c56b4f547aeef1
Nikko Adrian <nikkoadrian02@gmail.com>
2024-06-06 07:01:02 +0700

    a pdf

--- resources/views/bukti_pembayaran.blade.php
+++ resources/views/bukti_pembayaran.blade.php
@@ -85,10 +85,7 @@
                                     <p class="text-center"><b>Pada tanggal : {{ Carbon\Carbon::parse($pendaftaran->pendaftaran_created_at)->setTimezone('Asia/Jakarta')->translatedFormat('d F Y, H:i') }}</b></p>
                                     <p class="text-center">Simpan bukti pendaftaran ini. Sebagai syarat mengikuti proses recruitment perusahaan.</p>
                                     <p class="text-center m-3">{!! QrCode::size(100)->backgroundColor(255,255,255)->generate('https://bkk.smkmuhkandanghaur.sch.id/cari/'.$pendaftaran->code_pendaftaran) !!}</p>
-                                    <form action="download_pdf/{{ $pendaftaran -> code_pendaftaran }}" method="post">
-                                    @csrf
-                                        <p class="text-center"><button class="btn btn-primary" type="submit" >Download PDF</button></p>
-                                    </form>
+                                        <p class="text-center"><a class="btn btn-primary" target="_blank" href="/download_pdf/{{ $pendaftaran -> code_pendaftaran }}" >Download PDF</a></p>
                                             @php
                                                 $grupWaLink = $pendaftaran->grup_wa;
                                                 if (!preg_match("~^(?:f|ht)tps?://~i", $grupWaLink)) {

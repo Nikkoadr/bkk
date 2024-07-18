f9ee9d230c5aa6de9965977a221782ec62e56e23
Nikko Adrian <nikkoadrian02@gmail.com>
2024-07-18 10:32:50 +0700

    Update home.blade.php

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.main_admin')
+@section('link')
+<link rel="stylesheet" href="{{ asset('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+@endsection
 @section('content')
 <div class="content-wrapper">
     <div class="content-header">
@@ -19,7 +24,7 @@
     <section class="content">
         <div class="card">
             <div class="card-body">
-                <table class="table table-bordered">
+                <table class="table table-striped table-bordered">
                     <thead>
                         <tr>
                             <th>Nama PT</th>
@@ -43,4 +48,9 @@
         </div>
     </section>
 </div>
+@endsection
+@section('script')
+<script src="{{ asset('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
 @endsection

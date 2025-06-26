a296cca82c1119446666d5b8a193535676725764
Nikko Adrian <nikkoadrian02@gmail.com>
2025-06-26 08:51:40 +0700

    Update bukti_pembayaran.blade.php

--- resources/views/pendaftaran/bukti_pembayaran.blade.php
+++ resources/views/pendaftaran/bukti_pembayaran.blade.php
@@ -76,21 +76,22 @@
                                     <p class="text-center"><b>Perusahaan: {{ $pendaftaran-> nama_loker }}</b></p>
                                     <p class="text-center"><b>Pada tanggal : {{ Carbon\Carbon::parse($pendaftaran->pendaftaran_created_at)->setTimezone('Asia/Jakarta')->translatedFormat('d F Y, H:i') }}</b></p>
                                     <p class="text-center">Simpan bukti pendaftaran ini. Sebagai syarat mengikuti proses recruitment perusahaan.</p>
+                                    @php
+                                    $grupWaLink = $pendaftaran->grup_wa;
+                                    if (!preg_match("~^(?:f|ht)tps?://~i", $grupWaLink)) {
+                                        $grupWaLink = $grupWaLink;
+                                    }
+                                    @endphp
+                                    <p class="text-center"><a class="btn btn-success" href="{{ $grupWaLink }}" target="_blank">Gabung Grup Whatsapp</a></p>
+                                    <form action="print_bukti_transfer" method="post">
+                                        @csrf
+                                        <input type="hidden" name="code_pendaftaran" value="{{ $pendaftaran -> code_pendaftaran }}">
+                                        <div class="text-center m-2">
+                                            <button class="btn btn-primary" formtarget="_blank" type="submit">Cetak</button>
+                                        </div>
+                                    </form>
                                     <p class="text-center m-3">{!! QrCode::size(100)->backgroundColor(255,255,255)->generate('https://bkk.smkmuhkandanghaur.sch.id/scan/'.$pendaftaran->code_pendaftaran) !!}</p>
-                                        <form action="print_bukti_transfer" method="post">
-                                            @csrf
-                                            <input type="hidden" name="code_pendaftaran" value="{{ $pendaftaran -> code_pendaftaran }}">
-                                            <div class="text-center m-2">
-                                                <button class="btn btn-primary" formtarget="_blank" type="submit">Print</button>
-                                            </div>
-                                        </form>
-                                            @php
-                                                $grupWaLink = $pendaftaran->grup_wa;
-                                                if (!preg_match("~^(?:f|ht)tps?://~i", $grupWaLink)) {
-                                                    $grupWaLink = $grupWaLink;
-                                                }
-                                            @endphp
-                                    <p class="text-center"><a class="btn btn-success" href="{{ $grupWaLink }}" target="_blank">Join Grup Whatsapp</a></p>
+                                    
                                 </div>
                                 <!-- /.card-body -->
                             </div>
